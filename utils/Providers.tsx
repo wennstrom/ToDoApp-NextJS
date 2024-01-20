8c93a5dacb0629220f261eab8981b9a7006010bf
Oskar W <oskarw@Oskars-MacBook-Pro.local>
2024-01-20 12:24:33 +0100

    added devtools

--- utils/Providers.tsx
+++ utils/Providers.tsx
@@ -1,13 +1,14 @@
 'use client'
 import { ReactNode, useState } from "react"
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query"
-
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 export default function Provider ({children} : {children: ReactNode}) {
     const [queryClient] = useState(() => new QueryClient())
 
     return (
         <QueryClientProvider client={queryClient} >
             {children}
+            <ReactQueryDevtools />
         </QueryClientProvider>
     )
 }
